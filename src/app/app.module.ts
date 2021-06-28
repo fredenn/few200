674a3a2a264def151c04bf99f179ca96faf21215
fredenn <62033651+fredenn@users.noreply.github.com>
2021-06-28 12:39:15 -0400

    Before lunch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,22 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TodosComponent } from './components/todos/todos.component';
 import { HomeComponent } from './components/home/home.component';
-
+import { EntryComponent } from './components/todos/components/entry/entry.component';
+import { ListComponent } from './components/todos/components/list/list.component';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     TodosComponent,
-    HomeComponent
+    HomeComponent,
+    EntryComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/todos.models.ts
@@ -0,0 +1,4 @@
+export interface TodoListItemViewModel {
+  id: string;
+  description: string;
+}
